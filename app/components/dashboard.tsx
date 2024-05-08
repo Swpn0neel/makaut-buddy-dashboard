5297eebed96ed8ba8cc628feb891029031d0102b
Swapnoneel Saha <swapnoneelsaha111@gmail.com>
2024-05-08 22:37:30 +0530

    (feat): Added search bar and tabs

--- app/components/dashboard.tsx
+++ app/components/dashboard.tsx
@@ -1,27 +1,20 @@
 "use client";
 
 import React from "react";
-import { Tabs, Tab } from "@nextui-org/react";
+import SearchIcon from "./ui/searchicon";
+import Tabs from "./ui/tabs";
 import Card from "./ui/card";
 
 export default function dashboard() {
-  const variants = ["solid", "underlined", "bordered", "light"];
   return (
     <div className="h-full flex">
-      <div className="basis-1/12 bg-[#FFFFFF05] border-r-2 border-[#FFFFFF20]"></div>
-      <div className="basis-11/12 flex flex-col px-12 pt-32 pb-5">
+      <div className="basis-1/12 pt-28 bg-[#FFFFFF05] border-r-2 border-[#FFFFFF20]"></div>
+      <div className="basis-11/12 flex flex-col px-12 pt-28 pb-5 gap-2">
         <div className="text-6xl text-[#D9D9D9] font-medium">Dashboard</div>
-        {/* <div className="flex flex-wrap gap-4 w-full">
-          <Tabs
-            key="bordered"
-            variant="bordered"
-            aria-label="Tabs variants"
-            className="flex w-full rounded-none"
-          >
-            <Tab key="search" title="Search manually" className="" />
-            <Tab key="stream" title="Search by Stream" className="" />
-          </Tabs>
-        </div> */}
+        <Tabs />
+        <div className="w-full flex border-2 border-[#FFFFFF20] rounded-lg p-4">
+          <SearchIcon />
+        </div>
         <div className="h-full flex border-2 border-[#FFFFFF20] rounded-lg">
           <div className="basis-1/3"></div>
           <div className="basis-2/3 grid grid-cols-2 p-1">
