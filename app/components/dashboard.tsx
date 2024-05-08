afa0ce6ae5a1691d9a945e3a1eee81293c87c546
Swapnoneel Saha <swapnoneelsaha111@gmail.com>
2024-05-08 22:19:28 +0530

    random smth

--- app/components/dashboard.tsx
+++ app/components/dashboard.tsx
@@ -1,27 +1,32 @@
 "use client";
 
 import React from "react";
-import { Tabs, Tab } from "@nextui-org/react";
+import { SearchIcon } from "./SearchIcon";
 import Card from "./ui/card";
+import {
+  Tabs,
+  TabsList,
+  TabsTrigger,
+  TabsContent,
+} from "../../components/ui/tabs";
 
 export default function dashboard() {
-  const variants = ["solid", "underlined", "bordered", "light"];
   return (
-    <div className="h-full flex">
+    <div className="h-full flex bg-black">
       <div className="basis-1/12 bg-[#FFFFFF05] border-r-2 border-[#FFFFFF20]"></div>
-      <div className="basis-11/12 flex flex-col px-12 pt-32 pb-5">
+      <div className="basis-11/12 flex flex-col px-12 pt-32 pb-5 gap-2">
         <div className="text-6xl text-[#D9D9D9] font-medium">Dashboard</div>
-        {/* <div className="flex flex-wrap gap-4 w-full">
-          <Tabs
-            key="bordered"
-            variant="bordered"
-            aria-label="Tabs variants"
-            className="flex w-full rounded-none"
-          >
-            <Tab key="search" title="Search manually" className="" />
-            <Tab key="stream" title="Search by Stream" className="" />
+        <div className="w-full">
+          <Tabs defaultValue="account" className="w-[400px]">
+            <TabsList className="grid w-full grid-cols-2">
+              <TabsTrigger value="account">Account</TabsTrigger>
+              <TabsTrigger value="password">Password</TabsTrigger>
+            </TabsList>
           </Tabs>
-        </div> */}
+        </div>
+        <div className="w-full flex border-2 border-[#FFFFFF20] rounded-lg p-4">
+          <SearchIcon />
+        </div>
         <div className="h-full flex border-2 border-[#FFFFFF20] rounded-lg">
           <div className="basis-1/3"></div>
           <div className="basis-2/3 grid grid-cols-2 p-1">
